fc7cf5e7ab496f83258b48a316001b6eca73b6b8
Rushabh Parikh <RUSHABH.PARIKH1@baruchmail.cuny.edu>
2026-05-05 02:27:48 +0000

    final

--- models/project_work/marts/motor_veh_collision/dim_time.sql
+++ models/project_work/marts/motor_veh_collision/dim_time.sql
@@ -1,21 +1,26 @@
-WITH time AS (
+WITH time_values AS (
     SELECT DISTINCT 
-        CAST(PARSE_TIME('%H:%M', crash_time) AS TIME) AS crash_time  -- converting crash time to HH:MM:SS
-    FROM {{ref('staging_tbl_veh_collision')}}
+        CAST(PARSE_TIME('%H:%M', crash_time) AS TIME) AS crash_time_obj
+    FROM {{ ref('staging_tbl_veh_collision') }}
     WHERE crash_time IS NOT NULL 
+),
 
-)
-
-,dim_time AS (
+final_dim AS (
     SELECT 
-        {{ dbt_utils.generate_surrogate_key([
-            'EXTRACT(HOUR FROM crash_time)'
-            ,'EXTRACT(MINUTE FROM crash_time)'
-        ]) }} AS time_key
-        ,CAST(COALESCE(EXTRACT(HOUR FROM crash_time),0) AS INT)   AS hour -- extracting the hour from the time and replacing the NULLs with 0
-        ,CAST(COALESCE(EXTRACT(MINUTE FROM crash_time),0) AS INT) AS minute -- extracting the minute from the time and replacing the NULLs with 0
-    FROM time
+        crash_time_obj AS crash_time,
+        EXTRACT(HOUR FROM crash_time_obj) AS hour,
+        EXTRACT(MINUTE FROM crash_time_obj) AS minute,
+        -- Added for analytical value
+        CASE 
+            WHEN EXTRACT(HOUR FROM crash_time_obj) BETWEEN 6 AND 9 THEN 'Morning Rush'
+            WHEN EXTRACT(HOUR FROM crash_time_obj) BETWEEN 16 AND 19 THEN 'Evening Rush'
+            WHEN EXTRACT(HOUR FROM crash_time_obj) BETWEEN 0 AND 5 THEN 'Overnight'
+            ELSE 'Off-Peak'
+        END AS time_of_day_category
+    FROM time_values
 )
 
-SELECT *
-FROM dim_time
+SELECT 
+    {{ dbt_utils.generate_surrogate_key(['hour', 'minute']) }} AS time_key,
+    *
+FROM final_dim
--- models/project_work/marts/motor_veh_collision/fact_vehicle_collision.sql
+++ models/project_work/marts/motor_veh_collision/fact_vehicle_collision.sql
@@ -1,72 +1,70 @@
--- fact_vehicle_collisions: One row per collision event
--- Joins to Date, Time, Location, Casualty, and Contributing Factor dimensions
-
 WITH collisions AS (
-    -- Reference the project_work version of the staging table
-    -- We explicitly alias the casualty columns here to resolve the BigQuery 400 error
     SELECT 
         * EXCEPT(number_of_persons_injured, number_of_persons_killed),
-        CAST(number_of_persons_injured AS INT64) AS persons_injured,
-        CAST(number_of_persons_killed AS INT64) AS persons_killed
+        CAST(COALESCE(number_of_persons_injured, 0) AS INT64) AS persons_injured,
+        CAST(COALESCE(number_of_persons_killed, 0) AS INT64) AS persons_killed,
+        CAST(PARSE_TIME('%H:%M', crash_time) AS TIME) AS crash_time_obj
     FROM {{ ref('staging_tbl_veh_collision') }}
 ),
 
-dim_location AS (
-    SELECT * FROM {{ ref('dim_location') }}
-),
-
-dim_casualty AS (
-    SELECT * FROM {{ ref('dim_casualty') }}
-),
-
-dim_contributing_factor AS (
-    SELECT * FROM {{ ref('dim_contributing_factor') }}
-)
+-- Reference existing dimensions
+dim_location AS ( SELECT * FROM {{ ref('dim_location') }} ),
+dim_casualty AS ( SELECT * FROM {{ ref('dim_casualty') }} ),
+dim_con_factor AS ( SELECT * FROM {{ ref('dim_contributing_factor') }} ),
+dim_date AS ( SELECT * FROM {{ ref('dim_date') }} ),
+dim_time AS ( SELECT * FROM {{ ref('dim_time') }} )
 
 SELECT
     -- Primary Key
     {{ dbt_utils.generate_surrogate_key(['c.collision_id']) }} AS collision_key,
 
-    -- Foreign Keys (Generated to match dimension logic)
-    {{ dbt_utils.generate_surrogate_key(['c.crash_date']) }} AS crash_date_key,
-    
-    {{ dbt_utils.generate_surrogate_key([
-        'EXTRACT(HOUR FROM PARSE_TIME("%H:%M", c.crash_time))',
-        'EXTRACT(MINUTE FROM PARSE_TIME("%H:%M", c.crash_time))'
-    ]) }} AS crash_time_key,
-
+    -- Foreign Keys from Joined Dimensions
+    d.date_key AS crash_date_key,
+    t.time_key AS crash_time_key,
     l.location_key,
     ca.casualty_key,
     f.factor_key,
 
-    -- Degenerate Dimensions / Exact Location
-    c.exact_location
+    -- Measures & Coordinates
+    c.latitude,
+    c.longitude,
+    c.persons_injured,
+    c.persons_killed
 
 FROM collisions c
 
--- Join to Location using the alphabetical street sorting logic
+-- 1. Join to Date
+LEFT JOIN dim_date d 
+    ON CAST(c.crash_date AS DATE) = d.full_date
+
+-- 2. Join to Time (Simplified join on the TIME object)
+LEFT JOIN dim_time t 
+    ON c.crash_time_obj = t.crash_time
+
+-- 3. Join to Location (Handling the street sorting logic)
 LEFT JOIN dim_location l 
     ON c.borough = l.borough 
     AND c.zip_code = l.zip_code
-    AND (
-        CASE 
+    AND (CASE 
+            WHEN c.on_street_name IS NULL THEN c.off_street_name
+            WHEN c.off_street_name IS NULL THEN c.on_street_name
             WHEN c.on_street_name <= c.off_street_name THEN c.on_street_name 
             ELSE c.off_street_name 
-        END = l.cross_street_1
-    )
-    AND (
-        CASE 
+         END) = l.cross_street_1
+    AND (CASE 
+            WHEN c.on_street_name IS NULL OR c.off_street_name IS NULL THEN NULL
             WHEN c.on_street_name <= c.off_street_name THEN c.off_street_name 
             ELSE c.on_street_name 
-        END = l.cross_street_2
-    )
+         END) = l.cross_street_2
 
--- Join to Casualty (Now matches the aliases created in the CTE above)
+-- 4. Join to Casualty 
 LEFT JOIN dim_casualty ca 
     ON c.persons_injured = ca.persons_injured
     AND c.persons_killed = ca.persons_killed
 
--- Join to Contributing Factors
-LEFT JOIN dim_contributing_factor f 
-    ON c.factor_1 = f.factor_vehicle_1
-    AND c.factor_2 = f.factor_vehicle_2
+-- 5. Join to Contributing Factors (Corrected column names from staging)
+LEFT JOIN dim_con_factor f 
+    ON COALESCE(c.contributing_factor_vehicle_1, 'Unspecified') = f.factor_vehicle_1
+    AND COALESCE(c.contributing_factor_vehicle_2, 'Unspecified') = f.factor_vehicle_2
+    AND COALESCE(c.contributing_factor_vehicle_3, 'Unspecified') = f.factor_vehicle_3
+    AND COALESCE(c.contributing_factor_vehicle_4, 'Unspecified') = f.factor_vehicle_4
